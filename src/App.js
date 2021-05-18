f68d61d1bc4dbc8c4fc351ff9b7463831a6d6103
Greg Sonnier <gsonnier@comcast.net>
2021-05-18 11:26:15 -0600

    image assets brought over from original portfolio page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import profilePic from "./images/profile-pic.jpg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Container>
+			<h1>
+				<u>
+					About
+					Me&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				</u>
+			</h1>
+			<div>
+				<img alt="Greg smiling" src={profilePic} />
+				<span id="profileDesc">
+					I'm a full stack web developer from Aurora, Colorado. I have
+					experience with JavaScript, CSS, HTML, Java 8, and Python 3.
+					I've also worked as both a vocal coach teaching singing
+					lessons as well as a coding coach, teaching programming
+					fundamentals to kids K-12.
+					<br />
+					<br />
+					<br />
+					{/* <a href="./assets/Resume.pdf" className="menuLink">
+						My resume
+					</a> */}
+				</span>
+			</div>
+		</Container>
+	);
 }
 
 export default App;
