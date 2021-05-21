7ede927ae267d070c42a4f77c87cc6a8141a8f5f
Greg Sonnier <gsonnier@comcast.net>
2021-05-21 15:40:41 -0600

    moved About Me page to a component in preparation of using react router

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,15 +10,16 @@ export default class Header extends Component {
 						as={Navbar.Toggle}
 						className="navbarToggleBtn"
 					/>
+					<h5>Greg Sonnier, Web Developer</h5>
 					<Dropdown.Menu className="dropdownWindow">
-						<Dropdown.Item className="dropItem">
-							About Me
-						</Dropdown.Item>
 						<Dropdown.Item className="dropItem">
 							Portfolio
 						</Dropdown.Item>
 						<Dropdown.Item className="dropItem">
 							Contact
+						</Dropdown.Item>
+						<Dropdown.Item className="dropItem">
+							About Me
 						</Dropdown.Item>
 					</Dropdown.Menu>
 				</Dropdown>
