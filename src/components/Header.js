71543e44f0c12f872c912e318dd5adcdbfab0921
Greg Sonnier <gsonnier@comcast.net>
2021-05-21 14:15:53 -0600

    reworked navbar to use dropdown button without the arrow beside it

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
-import { Nav, Navbar } from "react-bootstrap";
+import { Dropdown, Navbar } from "react-bootstrap";
 
 export default class Header extends Component {
 	render() {
 		return (
 			<Navbar bg="light" expand="false">
-				<Navbar.Toggle className="navbarToggleBtn" />
-				<Navbar.Collapse>
-					<Nav>
-						<Nav.Link href="#">Test</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
+				<Dropdown>
+					<Dropdown.Toggle
+						as={Navbar.Toggle}
+						className="navbarToggleBtn e-caret-hide"
+					/>
+					<Dropdown.Menu>
+						<Dropdown.Item>Test</Dropdown.Item>
+					</Dropdown.Menu>
+				</Dropdown>
 			</Navbar>
 		);
 	}
