370a66f79eaed7ffe7ef6c125ddadddc943ece9b
Greg Sonnier <gsonnier@comcast.net>
2021-05-21 16:21:07 -0600

    basic routing using react-router-dom node package appears to be functioning correctly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import "./App.css";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
 
 function App() {
 	return (
 		<>
-			<Header />
-			<AboutMe />
+			<Router>
+				<Header />
+				<Switch>
+					<Route exact path="/">
+						<Portfolio />
+					</Route>
+					<Route path="/contact">
+						<Contact />
+					</Route>
+					<Route path="/about">
+						<AboutMe />
+					</Route>
+				</Switch>
+			</Router>
 		</>
 	);
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import { Dropdown, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export default class Header extends Component {
-	render() {
-		return (
-			<Navbar bg="light" expand="false" className="customNavbar">
-				<Dropdown>
-					<Dropdown.Toggle
-						as={Navbar.Toggle}
-						className="navbarToggleBtn"
-					/>
-					<h5>Greg Sonnier, Web Developer</h5>
-					<Dropdown.Menu className="dropdownWindow">
-						<Dropdown.Item className="dropItem">
-							Portfolio
-						</Dropdown.Item>
-						<Dropdown.Item className="dropItem">
-							Contact
-						</Dropdown.Item>
-						<Dropdown.Item className="dropItem">
-							About Me
-						</Dropdown.Item>
-					</Dropdown.Menu>
-				</Dropdown>
-			</Navbar>
-		);
-	}
+export default function Header() {
+	return (
+		<Navbar bg="light" expand="false" className="customNavbar">
+			<Dropdown>
+				<Dropdown.Toggle
+					as={Navbar.Toggle}
+					className="navbarToggleBtn"
+				/>
+				<h5>Greg Sonnier, Web Developer</h5>
+				<Dropdown.Menu className="dropdownWindow">
+					<Dropdown.Item className="dropItem">
+						<Link to="/">Portfolio</Link>
+					</Dropdown.Item>
+					<Dropdown.Item className="dropItem">
+						<Link to="/contact">Contact</Link>
+					</Dropdown.Item>
+					<Dropdown.Item className="dropItem">
+						<Link to="/about">About Me</Link>
+					</Dropdown.Item>
+				</Dropdown.Menu>
+			</Dropdown>
+		</Navbar>
+	);
 }
