dda8787ac99c37143a2886b23057902bfc357cde
Greg Sonnier <gsonnier@comcast.net>
2021-05-21 14:35:19 -0600

    further styled header to match general site theme

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,22 @@ import { Dropdown, Navbar } from "react-bootstrap";
 export default class Header extends Component {
 	render() {
 		return (
-			<Navbar bg="light" expand="false">
+			<Navbar bg="light" expand="false" className="customNavbar">
 				<Dropdown>
 					<Dropdown.Toggle
 						as={Navbar.Toggle}
-						className="navbarToggleBtn e-caret-hide"
+						className="navbarToggleBtn"
 					/>
-					<Dropdown.Menu>
-						<Dropdown.Item>Test</Dropdown.Item>
+					<Dropdown.Menu className="dropdownWindow">
+						<Dropdown.Item className="dropItem">
+							About Me
+						</Dropdown.Item>
+						<Dropdown.Item className="dropItem">
+							Portfolio
+						</Dropdown.Item>
+						<Dropdown.Item className="dropItem">
+							Contact
+						</Dropdown.Item>
 					</Dropdown.Menu>
 				</Dropdown>
 			</Navbar>
