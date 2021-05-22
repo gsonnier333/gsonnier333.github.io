6fc1a559377bbea7ffce8471cabe6422a9044648
Greg Sonnier <gsonnier@comcast.net>
2021-05-22 14:30:32 -0600

    footer bar added, includes the date of the most recent update to the portfolio

--- src/App.js
+++ src/App.js
@@ -4,23 +4,28 @@ import AboutMe from "./components/AboutMe";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
 		<>
 			<Router>
-				<Header />
-				<Switch>
-					<Route exact path="/">
-						<Portfolio />
-					</Route>
-					<Route path="/contact">
-						<Contact />
-					</Route>
-					<Route path="/about">
-						<AboutMe />
-					</Route>
-				</Switch>
+				<div id="wrapper">
+					<Header />
+
+					<Switch>
+						<Route exact path="/">
+							<Portfolio />
+						</Route>
+						<Route path="/contact">
+							<Contact />
+						</Route>
+						<Route path="/about">
+							<AboutMe />
+						</Route>
+					</Switch>
+					<Footer />
+				</div>
 			</Router>
 		</>
 	);
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,24 +5,27 @@ import Project from "./Projects/Project";
 
 export default function Portfolio() {
 	return (
-		<Container className="customCont">
-			<h1>
-				<u>
-					Portfolio&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-				</u>
-			</h1>
+		<>
+			<Container className="customCont">
+				<h1>
+					<u>
+						Portfolio&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					</u>
+				</h1>
 
-			<Project name="Meal Planner" />
+				<Project name="Meal Planner" />
 
-			<Project name="Password Generator" />
+				<Project name="Password Generator" />
 
-			<Project name="Weather Dashboard" />
+				<Project name="Weather Dashboard" />
 
-			<Project name="Galaxy Getaway" />
+				<Project name="Galaxy Getaway" />
 
-			<Project name="Day Planner" />
+				<Project name="Day Planner" />
 
-			<Project name="Employee Directory" />
-		</Container>
+				<Project name="Employee Directory" />
+			</Container>
+			<br />
+		</>
 	);
 }
